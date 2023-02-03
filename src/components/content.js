7a70736008230b32cccbd3b091ff3e726566d9b5
DESKTOP-J4A1050\kryst <Krystofbumbalek2@gmail.com>
2023-02-03 17:30:24 +0100

    update

--- src/components/content.js
+++ src/components/content.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 const Content = ({ data }) => {
-  return "sd";
-  // <ul>
-  //   {data.map((obj, index) => (
-  //     <li key={index}>{JSON.stringify(obj)}</li>
-  //   ))}
-  // </ul>
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <li>{JSON.stringify(item)}</li>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Content;
