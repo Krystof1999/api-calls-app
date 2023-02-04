bf135b73a0288f2c0b23533f87255918b8495014
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-02-04 19:22:20 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,27 +2,41 @@ import "./App.css";
 import TopButtons from "./components/top-buttons";
 import Content from "./components/content";
 import { useEffect, useState } from "react";
+import Table from "./components/table";
 
 function App() {
   const [data, setData] = useState([]);
+  const [apiError, setApiError] = useState(null);
+  const [btnIndex, setbtnIndex] = useState(1);
 
-  useEffect(() => {
-    const fetchPost = async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
+  const apiCall = async (url = "") => {
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok)
+        throw Error("Response is not ok => Check your URL adress!");
       const responseJson = await response.json();
       setData(responseJson);
-    };
-    fetchPost();
+    } catch (err) {
+      setApiError(err.message);
+    }
+  };
+
+  useEffect(() => {
+    apiCall("https://jsonplaceholder.typicode.com/users");
   }, []);
 
-  const handleClick = () => {};
+  const handleClick = (url, index) => {
+    apiCall(url);
+    setbtnIndex(index);
+  };
 
   return (
     <div className="App">
-      <TopButtons />
-      <Content data={data} />
+      <TopButtons handleClick={handleClick} btnIndex={btnIndex} />
+      {apiError && <p>{apiError}</p>}
+      {/* {!apiError && <Content data={data} />} */}
+      <Table data={data} />
     </div>
   );
 }
--- src/components/top-buttons.js
+++ src/components/top-buttons.js
@@ -1,11 +1,47 @@
 import React from "react";
 
-const TopButtons = () => {
+const TopButtons = ({
+  handleClickUsers,
+  handleClickPosts,
+  handleClickComments,
+  handleClick,
+  btnIndex,
+}) => {
   return (
     <div className="topButtons">
-      <button>users</button>
-      <button>posts</button>
-      <button>comments</button>
+      <button
+        onClick={() =>
+          handleClick("https://jsonplaceholder.typicode.com/users", 1)
+        }
+        style={{
+          backgroundColor: btnIndex === 1 ? "black" : "white",
+          color: btnIndex === 1 ? "white" : "black",
+        }}
+      >
+        users
+      </button>
+      <button
+        onClick={() =>
+          handleClick("https://jsonplaceholder.typicode.com/posts", 2)
+        }
+        style={{
+          backgroundColor: btnIndex === 2 ? "black" : "white",
+          color: btnIndex === 2 ? "white" : "black",
+        }}
+      >
+        posts
+      </button>
+      <button
+        onClick={() =>
+          handleClick("https://jsonplaceholder.typicode.com/comments", 3)
+        }
+        style={{
+          backgroundColor: btnIndex === 3 ? "black" : "white",
+          color: btnIndex === 3 ? "white" : "black",
+        }}
+      >
+        comments
+      </button>
     </div>
   );
 };
